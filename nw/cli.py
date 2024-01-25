5e9fbc8de486d1962d84c172d3868d16626fe85b
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2024-01-25 03:27:31 +0100

    Initial commit

---
+++ nw/cli.py
@@ -0,0 +1,70 @@
+import argparse
+
+import nw
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        description='Needleman-Wunsch algorithm')
+    parser.add_argument('-f1', '--fasta1', action='store',
+                        help='<Required> First sequence in fasta format',
+                        required=True)
+    parser.add_argument('-f2', '--fasta2', action='store',
+                        help='<Required> Second sequence in fasta format',
+                        required=True)
+    parser.add_argument('-b', '--blosum', action='store',
+                        help='<Required> BLOSUM matrix',
+                        required=True)
+    parser.add_argument('-p', '--gap-penalty', action='store', type=int,
+                        help='<Required> Gap penalty, e.g. -8',
+                        required=True)
+    parser.add_argument('--print-matrix', action='store_true',
+                        help='Print the synamic programming matrix')
+    results = parser.parse_args()
+
+    return {"fasta1": results.fasta1,
+            "fasta2": results.fasta2,
+            "blosum": results.blosum,
+            "gap_penalty": results.gap_penalty,
+            "print_matrix": results.print_matrix}
+
+
+def _print_matrix(m):
+    print(m)
+    print("")
+
+
+def _print_alignements(alignments, desc1, desc2):
+    print("Sequence 1: %s" % desc1)
+    print("Sequence 2: %s" % desc2)
+    print("")
+
+    for al1, al2 in alignments:
+        print(al1)
+        bars = ""
+        for i, x in enumerate(al1):
+            bars += ("|" if al2[i] == x else " ")
+        print(bars)
+        print(al2)
+        print("")
+
+
+def main():
+    args = _parse_args()
+
+    desc1, seq1 = nw.load_fasta(args["fasta1"])
+    desc2, seq2 = nw.load_fasta(args["fasta2"])
+    blosum = nw.load_blosum(args["blosum"])
+    gap_penalty = args["gap_penalty"]
+
+    m = nw.compute_matrix(seq1, seq2, blosum, gap_penalty)
+    alignments = nw.find_alignments(m, seq1, seq2, blosum, gap_penalty)
+
+    if args["print_matrix"]:
+        _print_matrix(m)
+
+    _print_alignements(alignments, desc1, desc2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ nw/nw.py
@@ -0,0 +1,92 @@
+import json
+import os
+
+import numpy as np
+
+
+def load_fasta(path):
+    with open(path, "r") as f:
+        desc = f.readline().strip()[1:]
+        seq = ""
+        for line in f:
+            seq += line.strip()
+
+    return desc, seq
+
+
+def load_blosum(blosum):
+    base_dir = os.path.dirname(__file__)
+    path = os.path.join(base_dir, "data", "%s.json" % blosum.lower())
+
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def _s(s, x, y):
+    return s[''.join(sorted((x, y)))]
+
+
+def compute_matrix(s1, s2, s, d):
+    m = np.zeros([len(s2) + 1, len(s1) + 1], dtype=np.int16)
+
+    m[0][0] = 0
+    for i in range(1, len(s1) + 1):
+        m[0][i] = m[0][i-1] + d
+    for i in range(1, len(s2) + 1):
+        m[i][0] = m[i-1][0] + d
+
+    for i, x in enumerate(s1, 1):
+        for j, y in enumerate(s2, 1):
+            f1 = m[j-1][i-1] + _s(s, x, y)
+            f2 = m[j][i-1] + d
+            f3 = m[j-1][i] + d
+            m[j][i] = max(f1, f2, f3)
+
+    return m
+
+
+def _find_alignments(m, s1, s2, s, d, alignments, i, j, al1, al2):
+    if i > 0 or j > 0:
+        x = s1[i-1]
+        y = s2[j-1]
+
+        if i > 0 and j > 0 and m[j][i] == m[j-1][i-1] + _s(s, x, y):
+            _find_alignments(m, s1, s2, s, d, alignments,
+                             i-1, j-1, x + al1, y + al2)
+        if i > 0 and m[j][i] == m[j][i-1] + d:
+            _find_alignments(m, s1, s2, s, d, alignments,
+                             i-1, j, x + al1, "-" + al2)
+        if j > 0 and m[j][i] == m[j-1][i] + d:
+            _find_alignments(m, s1, s2, s, d, alignments,
+                             i, j-1, "-" + al1, y + al2)
+    else:
+        alignments.append((al1, al2))
+
+
+def find_alignments(m, s1, s2, s, d):
+    alignments = []
+    i = len(s1)
+    j = len(s2)
+
+    _find_alignments(m, s1, s2, s, d, alignments, i, j, "", "")
+
+    return alignments
+
+
+def _create_blosum_json(txt_path, json_path):
+    """Convert a Blosum matrix in txt file format to json
+    """
+    m = []
+    with open(txt_path, "r") as f:
+        line = f.readline()
+        m.append([""] + line.split())
+        for line in f:
+            m.append(line.split())
+
+    h = {}
+    for i in range(1, len(m) - 1):
+        for j in range(1, i+1):
+            h[''.join(sorted((m[0][i] + m[j][0]).upper()))] = int(m[i][j])
+
+    with open(json_path, "w") as f:
+        json.dump(h, f, sort_keys=True, indent=4)
